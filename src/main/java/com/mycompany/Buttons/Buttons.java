36847f4a2dbb147b46fe5df229a7cd82803a7117
Aaron Maraya <aaronmaraya7@gmail.com>
2023-04-09 11:22:41 +0800

    Create arrayList and clearTextFields() for text fields
    -Buttons/Buttons.java

--- src/main/java/com/mycompany/Buttons/Buttons.java
+++ src/main/java/com/mycompany/Buttons/Buttons.java
@@ -2,14 +2,19 @@ package com.mycompany.Buttons;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Buttons extends JPanel{
     
+    private ArrayList<JTextField> textFields;
+
     public Buttons(JFrame frame) {
         add(new JButton("Print"));
+        
         JButton clear = new JButton("Clear");
         add(clear);
             
@@ -22,5 +27,10 @@ public class Buttons extends JPanel{
             });
             add(exit);
             
+    }
+    public void clearTextFields() {
+        for (JTextField textField : textFields) {
+            textField.setText("");
+        }
     }
 }
