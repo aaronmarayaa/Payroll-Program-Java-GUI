4bea1fed688ffe5fea8d52bdb3514d0766749e0b
Aaron Maraya <aaronmaraya7@gmail.com>
2023-04-12 20:16:58 +0800

    Remove comments
    -Informations/InformationPanel.java

--- src/main/java/com/mycompany/Informations/InformationPanel.java
+++ src/main/java/com/mycompany/Informations/InformationPanel.java
@@ -12,21 +12,24 @@ public class InformationPanel extends JPanel {
     public InformationPanel() {
         
         setLayout(new BorderLayout());
+        
+        // Title date panel
         TitleDatePanel titleDatePanel = new TitleDatePanel();
         add(titleDatePanel, BorderLayout.NORTH);
         
+        // employee information panel
         employeeInformation = new EmployeeInformation();
         JPanel employeeInfo_panel = new JPanel(new BorderLayout());
             employeeInfo_panel.add(employeeInformation);
         add(employeeInfo_panel, BorderLayout.CENTER);
         
+        // salary information panel
         salaryInformation = new SalaryInformation();
         JPanel salaryInfo_panel = new JPanel(new BorderLayout());
             salaryInfo_panel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
             salaryInfo_panel.add(salaryInformation);
         add(salaryInfo_panel, BorderLayout.SOUTH);
     }
-    // get `salaryInformation` making it accessible in `Computation` Panel
     public static final SalaryInformation getSalaryInformationPanel() {
         return salaryInformation;
     }
